8a3b7124637e3915525b62843fce159902c00b05
BobbyMoreau <bobbymoreauraquin@gmail.com>
2025-04-09 14:18:02 +0200

    added test

---
+++ test/testCounter.test.ts
@@ -0,0 +1,24 @@
+import { setupCounter } from '../src/counter'
+import { expect, test } from 'vitest'
+
+test('setupCounter increments the counter', () => {
+  let html = ''
+  const mockElement = {
+    innerHTML: '',
+    addEventListener: (_: string, cb: () => void) => {
+      mockElement.click = cb
+    },
+    click: () => {},
+  }
+
+  // @ts-expect-error we’re faking just enough for the test
+  setupCounter(mockElement)
+
+  expect(mockElement.innerHTML).toBe('count is 0')
+
+  mockElement.click()
+  expect(mockElement.innerHTML).toBe('count is 1')
+
+  mockElement.click()
+  expect(mockElement.innerHTML).toBe('count is 2')
+})
